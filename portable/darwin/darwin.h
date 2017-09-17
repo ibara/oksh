d0301f9572a4739b12e551938752ea1195aebb69
Brian Callahan <dodonpachi-github@mailinator.com>
2017-09-17 17:38:34 -0400

    Add timespeccmp and timespecsub to all the portable includes.

--- portable/darwin/darwin.h
+++ portable/darwin/darwin.h
@@ -13,5 +13,22 @@
 #define setresgid(x, y, z)	setgid(x); setegid(y); setgid(z)
 #define setresuid(x, y, z)	setuid(x); seteuid(y); setuid(z)
 
+/* From OpenBSD sys/time.h */
+#define timespeccmp(tsp, usp, cmp)                                      \
+        (((tsp)->tv_sec == (usp)->tv_sec) ?                             \
+            ((tsp)->tv_nsec cmp (usp)->tv_nsec) :                       \
+            ((tsp)->tv_sec cmp (usp)->tv_sec))
+
+#define timespecsub(tsp, usp, vsp)                                      \
+        do {                                                            \
+                (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;          \
+                (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;       \
+                if ((vsp)->tv_nsec < 0) {                               \
+                        (vsp)->tv_sec--;                                \
+                        (vsp)->tv_nsec += 1000000000L;                  \
+                }                                                       \
+        } while (0)
+
+/* Functions */
 long long strtonum(const char *numstr, long long minval, long long maxval,
 		   const char **errstrp);
--- portable/netbsd/netbsd.h
+++ portable/netbsd/netbsd.h
@@ -6,3 +6,19 @@
 #define setresgid(x, y, z)	setgid(x); setegid(x)
 #define setresuid(x, y, z)	setuid(x); seteuid(x)
 #define srand_deterministic(x)	srand(x)
+
+/* From OpenBSD sys/time.h */
+#define timespeccmp(tsp, usp, cmp)                                      \
+        (((tsp)->tv_sec == (usp)->tv_sec) ?                             \
+            ((tsp)->tv_nsec cmp (usp)->tv_nsec) :                       \
+            ((tsp)->tv_sec cmp (usp)->tv_sec))
+
+#define timespecsub(tsp, usp, vsp)                                      \
+        do {                                                            \
+                (vsp)->tv_sec = (tsp)->tv_sec - (usp)->tv_sec;          \
+                (vsp)->tv_nsec = (tsp)->tv_nsec - (usp)->tv_nsec;       \
+                if ((vsp)->tv_nsec < 0) {                               \
+                        (vsp)->tv_sec--;                                \
+                        (vsp)->tv_nsec += 1000000000L;                  \
+                }                                                       \
+        } while (0)
