e6f14a4dd2f4cbb86c9ad17fc0e1e3566a4d0552
Brian Callahan <bcallah@openbsd.org>
2023-04-16 19:04:35 -0400

    oksh-7.3

--- README.md
+++ README.md
@@ -139,5 +139,5 @@ for details.
 
 Get a tarball
 -------------
-See releases tab. The latest release is oksh-7.2, which matches the ksh(1)
-from OpenBSD 7.2, released October 20, 2022.
+See releases tab. The latest release is oksh-7.3, which matches the ksh(1)
+from OpenBSD 7.3, released April 10, 2023.
--- main.c
+++ main.c
@@ -308,7 +308,7 @@ main(int argc, char *argv[])
 #ifndef SMALL
 	/* setstr can't fail here */
 	setstr(global(version_param), ksh_version, KSH_RETURN_ERROR);
-	setstr(global("OKSH_VERSION"), "oksh 7.2", KSH_RETURN_ERROR);
+	setstr(global("OKSH_VERSION"), "oksh 7.3", KSH_RETURN_ERROR);
 #endif /* SMALL */
 
 	/* execute initialization statements */
